4e14e54ad5c2127e9a6ce9356f16921bd8bd26a8
Aditya Rai <adityarai29052005@gmail.com>
2025-11-20 21:48:54 +0530

    updated

--- data/testimonial.js
+++ data/testimonial.js
@@ -2,25 +2,25 @@ export const testimonial = [
   {
     quote:
       "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
-    author: "Sarah Chen",
-    image: "https://randomuser.me/api/portraits/women/75.jpg",
+    author: "Komal Pandey",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQEzBA4fZBtbUA/profile-displayphoto-shrink_800_800/B56Zdv0byiGoAc-/0/1749927708057?e=1765411200&v=beta&t=i9OKdkqxaPLbHn75poUcjveu1KyAJWQoR1ZBtH1ksY0",
     role: "Software Engineer",
-    company: "Tech Giant Co.",
+    company: "Infosys.",
   },
   {
     quote:
       "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-    author: "Michael Rodriguez",
-    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    author: "Utkarsh Tiwari",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQEsnatuvo1fJw/profile-displayphoto-shrink_800_800/B56ZPfuvONHIAc-/0/1734625393384?e=1765411200&v=beta&t=XVV_cKGN8hnJ0Gl1jVtzc_OchZLbnvqz0qhPF8sHFXc",
     role: "Product Manager",
-    company: "StartUp Inc.",
+    company: "Wipro.",
   },
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
-    author: "Priya Patel",
-    image: "https://randomuser.me/api/portraits/women/74.jpg",
-    role: "Marketing Director",
-    company: "Global Corp",
+    author: "Ayush Pandey",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQEXAV4EQWGyAQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1732701254427?e=1765411200&v=beta&t=PIiL-TqxsPYq4D8OM9ZZPU5JKR-Bb-TjIvR9XiG-mDQ",
+    role: "AIML Engineer",
+    company: "Microsoft",
   },
 ];
