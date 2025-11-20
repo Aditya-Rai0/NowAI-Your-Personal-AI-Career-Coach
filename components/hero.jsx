806850a8b692c3a34c7a487c2b77d3d17217d783
Aditya Rai <adityarai29052005@gmail.com>
2025-11-20 19:39:26 +0530

    Updated

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://youtu.be/gbvmP5w-xhM?si=s-Pi0U0tEZHzOh1k">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
